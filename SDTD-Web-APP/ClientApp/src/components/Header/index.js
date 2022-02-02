62c11c9d58c08d6de03b255bcbc62f3ac4925551
AgonneyUbt <aa47501@ubt-uni.net>
2022-02-02 19:03:11 +0100

    updated commit

--- SDTD-Web-APP/ClientApp/src/components/Header/index.js
+++ SDTD-Web-APP/ClientApp/src/components/Header/index.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
+import Video from '../../videos/Video.mp4';
 import {Button} from '../ButtonElement';
 import {
      HeaderContainer,
      HeaderBg, 
+     VideoBg,
      HeaderContent, 
      HeaderH1, 
      HeaderP, 
      HeaderBtnWrapper,
-    ArrowForward,
-    ArrowRight  } from './HeaderElements';
+     ArrowForward,
+     ArrowRight  } from './HeaderElements';
 
 const Header = () => {
     const [hover, setHover] = useState(false)
@@ -20,18 +22,23 @@ const Header = () => {
   return (
     <HeaderContainer>
         <HeaderBg>
-            <HeaderContent>
+            <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+        </HeaderBg>
+        <HeaderContent>
                 <HeaderH1>Tema e diplomes made easy</HeaderH1>
                 <HeaderP> Krijoni nje llogari sot qe te keni nje proces sa me te thjeshte te zhvillimit te temes se diplomes.</HeaderP>
                 <HeaderBtnWrapper>
-                    <Button to="singup" onMouseEnter={onHover}
-                    onMouseLeave={onHover}>
+                    <Button
+                     to="singup" 
+                     onMouseEnter={onHover}
+                     onMouseLeave={onHover}
+                     primary='true'
+                     dark='true'
+                    >
                         Get started {hover ? <ArrowForward/> : <ArrowRight/>}
                     </Button>
                 </HeaderBtnWrapper>
-            </HeaderContent>
-
-        </HeaderBg>
+        </HeaderContent>
 
     </HeaderContainer> 
   );
